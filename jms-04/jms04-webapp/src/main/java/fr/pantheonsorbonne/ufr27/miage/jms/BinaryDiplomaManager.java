eae9347664cd23e831fdb6ba05e0462ec258ce59
LdvcJcb <33324117+LdvcJcb@users.noreply.github.com>
2020-01-06 16:20:41 +0100

    Update BinaryDiplomaManager.java
    BinDip functions

--- jms-04/jms04-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/jms/BinaryDiplomaManager.java
+++ jms-04/jms04-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/jms/BinaryDiplomaManager.java
@@ -2,11 +2,13 @@ package fr.pantheonsorbonne.ufr27.miage.jms;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.jms.BytesMessage;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
@@ -14,6 +16,9 @@ import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 import fr.pantheonsorbonne.ufr27.miage.DiplomaInfo;
 import fr.pantheonsorbonne.ufr27.miage.dto.BinaryDiplomaDTO;
@@ -60,7 +65,21 @@ public class BinaryDiplomaManager implements Closeable {
 		// create a BinaryDiplomaDTO containing the id of the diploma and the data
 		// return the DTO
 
-		return null;
+		try {
+			BytesMessage msg = (BytesMessage)binDiplomaConsumer.receive();
+			byte[] data = new byte[(int) msg.getBodyLength()];
+			msg.readBytes(data);
+			BinaryDiplomaDTO binDip = new BinaryDiplomaDTO ();
+			binDip.setId(msg.getIntProperty("id"));
+			binDip.setData(data);
+			return binDip;
+
+		} catch (JMSException e) {
+			System.out.println("FAILED TO CONSUME THE MESSAGE");
+			return null;
+		}
+
+		
 	}
 
 	public void requestBinDiploma(DiplomaInfo info) {
@@ -69,7 +88,14 @@ public class BinaryDiplomaManager implements Closeable {
 		// create a JaxBContext, and bount DiplomaInfo.class
 		// create a Marshaller and marshall the class in the writer
 		// send a text Message containing the JAXB-marshalled object through the wire
-
+		try {
+			StringWriter sw = new StringWriter();
+			JAXBContext jxbc = JAXBContext.newInstance(DiplomaInfo.class);
+			jxbc.createMarshaller().marshal(info, sw);
+			this.diplomaRequestProducer.send(this.session.createTextMessage(sw.toString()));
+		} catch (JAXBException | JMSException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Request failed");		}	
 	}
 
 	@Override
