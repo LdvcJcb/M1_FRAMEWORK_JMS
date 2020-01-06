d9694243367d7b1a634c043b3a55520b0d8dc2cd
LdvcJcb <33324117+LdvcJcb@users.noreply.github.com>
2020-01-06 16:18:37 +0100

    Update PdfGeneratorMessageHandler.java
    PDF functions

--- jms-04/jms04-pdfgenerator/src/main/java/fr/pantheonsorbonne/miage/jms/PdfGeneratorMessageHandler.java
+++ jms-04/jms04-pdfgenerator/src/main/java/fr/pantheonsorbonne/miage/jms/PdfGeneratorMessageHandler.java
@@ -1,12 +1,14 @@
-package fr.pantheonsorbonne.miage.jms;
+package fr.pantheonsorbonne.ufr27.miage.jms;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.jms.BytesMessage;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
@@ -14,11 +16,15 @@ import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 import fr.pantheonsorbonne.ufr27.miage.DiplomaInfo;
+import fr.pantheonsorbonne.ufr27.miage.dto.BinaryDiplomaDTO;
 
 @ApplicationScoped
-public class PdfGeneratorMessageHandler implements Closeable {
+public class BinaryDiplomaManager implements Closeable {
 
 	@Inject
 	@Named("diplomaRequests")
@@ -32,8 +38,8 @@ public class PdfGeneratorMessageHandler implements Closeable {
 	private ConnectionFactory connectionFactory;
 
 	private Connection connection;
-	private MessageConsumer diplomaRequestConsummer;
-	private MessageProducer diplomaFileProducer;
+	private MessageConsumer binDiplomaConsumer;
+	private MessageProducer diplomaRequestProducer;
 
 	private Session session;
 
@@ -43,49 +49,59 @@ public class PdfGeneratorMessageHandler implements Closeable {
 			connection = connectionFactory.createConnection("nicolas", "nicolas");
 			connection.start();
 			session = connection.createSession();
-			diplomaRequestConsummer = session.createConsumer(requestsQueue);
-			diplomaFileProducer = session.createProducer(filesQueue);
-
+			binDiplomaConsumer = session.createConsumer(filesQueue);
+			diplomaRequestProducer = session.createProducer(requestsQueue);
 		} catch (JMSException e) {
 			throw new RuntimeException(e);
 		}
 
 	}
 
-	public void consume() {
-		
-		// receive a text message from the consummer
-		// create a jaxbcontext, binding the DiplomaInfo class
-		// unmarshall the texte message body with the JaxBcontext
-		// use the handleReceivedDiplomaSpect method to generate the diploma an send it through the wire
+	public BinaryDiplomaDTO consume() {
+
+		// receive a Byte Message from the consumer
+		// create a byte array sized after the message's payload body length
+		// read the message on the byte array
+		// create a BinaryDiplomaDTO containing the id of the diploma and the data
+		// return the DTO
+
+		try {
+			BytesMessage msg = (BytesMessage)binDiplomaConsumer.receive();
+			byte[] data = new byte[(int) msg.getBodyLength()];
+			msg.readBytes(data);
+			BinaryDiplomaDTO binDip = new BinaryDiplomaDTO ();
+			binDip.setId(msg.getIntProperty("id"));
+			binDip.setData(data);
+			return binDip;
+
+		} catch (JMSException e) {
+			System.out.println("FAILED TO CONSUME THE MESSAGE");
+			return null;
+		}
+
 		
 	}
 
-	private void handledReceivedDiplomaSpect(DiplomaInfo diploma) {
+	public void requestBinDiploma(DiplomaInfo info) {
 
-		// create a new MIageDiplomaGenerator Instance from the diploma Info
-		// get the content (inputstream ) from the generator
-		// create an array of bytes having the size of the inputstream
-		// read the inputstream data into the adday
-		// use the sendBinary sendBinaryDiploma function to send the diploma through the
-		// write
-		// close the IS
-
-	}
-
-	public void sendBinaryDiploma(DiplomaInfo info, byte[] data) {
-		//	create a new byte message using the session object
-		// set an IntProperty on the message containing the id of the diploma
-		// write the bytes into the bytesmessage
-		// send the message through the producer
-
+		// create a String writer
+		// create a JaxBContext, and bount DiplomaInfo.class
+		// create a Marshaller and marshall the class in the writer
+		// send a text Message containing the JAXB-marshalled object through the wire
+		try {
+			StringWriter sw = new StringWriter();
+			JAXBContext jxbc = JAXBContext.newInstance(DiplomaInfo.class);
+			jxbc.createMarshaller().marshal(info, sw);
+			this.diplomaRequestProducer.send(this.session.createTextMessage(sw.toString()));
+		} catch (JAXBException | JMSException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Request failed");		}	
 	}
 
 	@Override
 	public void close() throws IOException {
 		try {
-			diplomaFileProducer.close();
-			diplomaRequestConsummer.close();
+			binDiplomaConsumer.close();
 			session.close();
 			connection.close();
 		} catch (JMSException e) {
